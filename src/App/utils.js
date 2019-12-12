e190904b0b6747f35f91cb3beaf214669dab1699
Gonzalo Rafael Acosta <gacosta.r89@gmail.com>
2019-12-12 19:49:43 -0300

    [redux-mvc] Avoid running multiple saga middlewares in module
    - Check if a saga middleware already exists in the module.
      If exists replace it for the new one

--- src/App/utils.js
+++ src/App/utils.js
@@ -3,13 +3,30 @@ import createSagaMiddleware from "redux-saga"
 import * as R from "ramda"
 import { addCreateStore, addReducer, addEvents } from "redux-mvc"
 
+const isSagaMiddleware = Symbol("isSagaMiddleware")
+
 export const addSagaMiddleware = rootSaga => module => {
+    const middleware = module.middleware || []
+
+    const index = middleware.findIndex(
+        middleware => middleware[isSagaMiddleware]
+    )
+
     const sagaMiddleware = createSagaMiddleware()
+
+    sagaMiddleware[isSagaMiddleware] = true
 
     const newModule = {
         ...module,
         sagas: [...(module.sagas || []), rootSaga],
-        sagaMiddleware,
+        middleware:
+            typeof index === "undefined"
+                ? [...middleware, sagaMiddleware]
+                : [
+                      ...middleware.slice(0, index),
+                      sagaMiddleware,
+                      ...middleware.slice(index + 1),
+                  ],
     }
 
     newModule.on("start", () => {
