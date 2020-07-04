becb5e86c29a7a8ef11b26ffb6ba503c7c72bfdd
Gonzalo Rafael Acosta <gacosta.r89@gmail.com>
2020-07-04 18:50:46 -0300

    [documentation] Remove storybook

--- .storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require("../webpack.dev.js")
-
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config, mode }) => ({
-    ...config,
-    resolve: {
-        modules: baseConfig.resolve.modules,
-    },
-})
